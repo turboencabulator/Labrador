6cd2213b0d11d0c72a1a553c05e90550bc3048e7
Kyle Guinn <elyk03@gmail.com>
2025-03-03 22:48:10 -0600

    Set spinbox min/max simultaneously

--- Desktop_Interface/mainwindow.cpp
+++ Desktop_Interface/mainwindow.cpp
@@ -265,12 +265,10 @@ MainWindow::MainWindow(QWidget *parent) :
 
     spectrumLayoutWidget->setLayout(spectrumLayout);
     spectrumMinXSpinbox->setSuffix(QString::fromUtf8("Hz"));
-    spectrumMinXSpinbox->setMinimum(0);
-    spectrumMinXSpinbox->setMaximum(187500);
+    spectrumMinXSpinbox->setRange(0, 187500);
     spectrumMinXSpinbox->setSingleStep(1000);
     spectrumMaxXSpinbox->setSuffix(QString::fromUtf8("Hz"));
-    spectrumMaxXSpinbox->setMinimum(0);
-    spectrumMaxXSpinbox->setMaximum(187500);
+    spectrumMaxXSpinbox->setRange(0, 187500);
     spectrumMaxXSpinbox->setValue(187500);
     spectrumMaxXSpinbox->setSingleStep(10000);
 
@@ -311,13 +309,11 @@ MainWindow::MainWindow(QWidget *parent) :
 
     freqRespLayout1Widget->setLayout(freqRespLayout1);
     freqRespMinXSpinbox->setSuffix(QString::fromUtf8("Hz"));
-    freqRespMinXSpinbox->setMinimum(100);
-    freqRespMinXSpinbox->setMaximum(62500);
+    freqRespMinXSpinbox->setRange(100, 62500);
     freqRespMinXSpinbox->setValue(100);
     freqRespMinXSpinbox->setSingleStep(10);
     freqRespMaxXSpinbox->setSuffix(QString::fromUtf8("Hz"));
-    freqRespMaxXSpinbox->setMinimum(100);
-    freqRespMaxXSpinbox->setMaximum(62500);
+    freqRespMaxXSpinbox->setRange(100, 62500);
     freqRespMaxXSpinbox->setValue(32500);
     freqRespMaxXSpinbox->setSingleStep(1000);
 
@@ -331,8 +327,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     freqRespLayout2Widget->setLayout(freqRespLayout2);
     freqRespStepSpinbox->setSuffix(QString::fromUtf8("Hz"));
-    freqRespStepSpinbox->setMinimum(10);
-    freqRespStepSpinbox->setMaximum(10000);
+    freqRespStepSpinbox->setRange(10, 10000);
     freqRespStepSpinbox->setValue(100);
     freqRespStepSpinbox->setSingleStep(10);
     freqRespTypeComboBox->addItem("Gain");
