3139b01a94e562e370bf5da021281ac11c783c54
Kyle Guinn <elyk03@gmail.com>
2025-05-21 01:37:48 -0500

    Remove the last of the sprintf calls

--- Desktop_Interface/ui_elements/siprint.cpp
+++ Desktop_Interface/ui_elements/siprint.cpp
@@ -9,51 +9,27 @@ siprint::siprint(const char *unitsInit, double valInit)
 }
 
 char* siprint::printVal(){
-    bool negative = (value < 0);
+    const char *sign = (value < 0.0) ? "-" : "";
     double abs_value = std::fabs(value);
 
-    char* tempStringPtr = printString;
-    if (negative)
-    {
-        printString[0] = '-';
-        tempStringPtr++;
-    }
-
-    if (abs_value >= 1000000000000000000)
-    {
-        sprintf(tempStringPtr, "Inf %s", units);
-    }
-    else if (abs_value >= 1000000)
-    {
-        sprintf(tempStringPtr, "%.2fM%s", abs_value/1000000, units);
-    }
-    else if (abs_value >= 1000)
-    {
-        sprintf(tempStringPtr, "%.2fk%s", abs_value/1000, units);
-    }
-    else if (abs_value >= 1)
-    {
-        sprintf(tempStringPtr, "%.2f%s", abs_value, units);
-    }
-    else if (abs_value >= 0.001)
-    {
-        sprintf(tempStringPtr, "%.2fm%s", abs_value*1000, units);
-    }
-    else if (abs_value >= 0.000001)
-    {
-        sprintf(tempStringPtr, "%.2fu%s", abs_value*1000000, units);
-    }
-    else if (abs_value >= 0.000000001)
-    {
-        sprintf(tempStringPtr, "%.2fn%s", abs_value*1000000000, units);
-    }
-    else if (abs_value >= 0.000000000001)
-    {
-        sprintf(tempStringPtr, "%.2fp%s", abs_value*1000000000000, units);
-    }
-    else
-    {
-        sprintf(tempStringPtr, "%.2f%s", abs_value, units);
+    if (abs_value >= 1000000000000000000.0) {
+        snprintf(printString, sizeof printString, "%sInf %s", sign, units);
+    } else if (abs_value >= 1000000.0) {
+        snprintf(printString, sizeof printString, "%s%.2fM%s", sign, abs_value/1000000, units);
+    } else if (abs_value >= 1000.0) {
+        snprintf(printString, sizeof printString, "%s%.2fk%s", sign, abs_value/1000, units);
+    } else if (abs_value >= 1.0) {
+        snprintf(printString, sizeof printString, "%s%.2f%s", sign, abs_value, units);
+    } else if (abs_value >= 0.001) {
+        snprintf(printString, sizeof printString, "%s%.2fm%s", sign, abs_value*1000, units);
+    } else if (abs_value >= 0.000001) {
+        snprintf(printString, sizeof printString, "%s%.2fu%s", sign, abs_value*1000000, units);
+    } else if (abs_value >= 0.000000001) {
+        snprintf(printString, sizeof printString, "%s%.2fn%s", sign, abs_value*1000000000, units);
+    } else if (abs_value >= 0.000000000001) {
+        snprintf(printString, sizeof printString, "%s%.2fp%s", sign, abs_value*1000000000000, units);
+    } else {
+        snprintf(printString, sizeof printString, "%s%.2f%s", sign, abs_value, units);
     }
 
     return printString;
